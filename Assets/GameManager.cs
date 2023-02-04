d89db63b4064c96955351391970e8408a9f98866
FedericoCroci <fedecroci@hotmail.com>
2023-02-04 16:02:07 -0300

    timer

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public Slider progressBar;
     private float maxValue = 100;
     public float progress;
+	public TextMeshProUGUI text;
+    public float totalProgress = 0;
 
     private void Update()
     {
-        progressBar.value += Mathf.Clamp((progress * Time.deltaTime) / 100, 0, maxValue);
+        totalProgress += Mathf.Clamp((progress * Time.deltaTime) / 100, 0, maxValue);
+        text.text = (totalProgress).ToString();
     }
 }
