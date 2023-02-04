4ce79e85b238e24e1319442ac7c7e3ddcaefe701
chars <charsdeveloper@gmail.com>
2023-02-04 16:31:51 -0300

    Camera Confiner

---
+++ Assets/AttachComponent.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Cinemachine;
+
+public class AttachComponent : MonoBehaviour
+{
+    public Collider bounding;
+    public CinemachineConfiner cinemachineConfiner;
+
+    private void LateUpdate()
+    {
+        if (bounding == null)
+        {
+            bounding = GameObject.FindGameObjectWithTag("Bounding").GetComponent<Collider>();
+            cinemachineConfiner.m_BoundingVolume = bounding;
+        }      
+    }
+}
