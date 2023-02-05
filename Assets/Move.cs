545159a74938e97952d5cec21331c96b5f326c4c
Federico Croci <fedecroci@hotmail.com>
2023-02-05 12:38:27 -0300

    attacks fire

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -8,16 +8,13 @@ public class Move : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private Camera _camera;
+    [SerializeField] private GameObject _magicHint;
     private Vector3 _destination;
-    private bool _moving;
     private MagicController magicController;
     public Vector3 OffsetMagic;
 
-    public bool Moving { get => _moving; }
-
     private void Start()
     {
-        _moving = false;
         magicController = GameObject.FindAnyObjectByType<MagicController>();
     }
 
@@ -25,71 +22,30 @@ public class Move : MonoBehaviour
     {
         var movementDir = (new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"))).normalized;
         transform.position += movementDir * _speed * Time.deltaTime;
-
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Fire")){
             Debug.Log("its FIRE!");
+            _magicHint.SetActive(true);
         }
-
-
     }
 
-    private void GetPointToMove()
+    void OnTriggerStay(Collider other)
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                _destination = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-
-                if (magicController.MagicType != string.Empty)
-                {
-                    _destination -= OffsetMagic;
-                }
-
-                _moving = true;
-                Debug.Log(hit.point);
-                Debug.Log(hit.collider.gameObject.name);
+        if(other.gameObject.CompareTag("Fire")){
+            if(Input.GetKeyDown("space")){
+                Destroy(other.gameObject);
+                _magicHint.SetActive(false);
             }
         }
     }
 
-    public void MoveToPoint()
+    void OnTriggerExit(Collider other)
     {
-        if (_moving)
-        {
-            transform.position = Vector3.Lerp(transform.position, _destination, Time.deltaTime * _speed);
-            if (Approximately(transform.position, _destination, 0.001f))
-            {
-                transform.position = _destination;
-                _moving = false;
-
-                if (magicController.MagicType != string.Empty)
-                {
-                    Debug.Log("Enter here");
-                    magicController.UseMagic();
-                }
-            }
+        if(other.gameObject.CompareTag("Fire")){
+            _magicHint.SetActive(false);
         }
-    }
-
-
-    public bool Approximately(Vector3 me, Vector3 other, float percentage)
-    {
-        var dx = me.x - other.x;
-        if (Mathf.Abs(dx) > me.x * percentage)
-            return false;
-
-        var dy = me.y - other.y;
-        if (Mathf.Abs(dy) > me.y * percentage)
-            return false;
-
-        var dz = me.z - other.z;
-        return Mathf.Abs(dz) >= me.z * percentage;
     }
 }
